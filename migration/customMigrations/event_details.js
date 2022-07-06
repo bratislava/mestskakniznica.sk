6975b2061c214ac66f6c3f3183b32a5f4a06fbb4
mohsen <mohsen@kernadler.com>
2022-07-06 12:22:32 +0430

    fixed relations

--- migration/customMigrations/event_details.js
+++ migration/customMigrations/event_details.js
@@ -2,15 +2,14 @@ const { migrate } = require('../migrate/helpers/migrate')
 
 // Tables that should not be proccessed later
 const processedTables = []
-// const processedTables = ['file_category']
 
 // Custom migration function, handles DB reads and writes
 async function migrateTables() {
-  // await migrate('file_category', 'file_categories')
-  // await migrate('file_category', 'file_categories_page_links',(role) => ({
-  //   faq_section_id: role.id,
-  //   page_id: role.faqSectionRedirectTo,
-  // }))
+  await migrate('components_sections_event_details__event_tags', 'components_sections_event_details_event_tags_links',(role) => ({
+    event_details_id: role['components_sections_event_detail_id'],
+    event_tag_id: role['event-tag_id'],
+  }))
+
 }
 
 module.exports = {
