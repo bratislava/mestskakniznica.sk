8e4c996871e4c1dc6b18b3aebd88a8879cdc1803
hlodnb <francviktor@gmail.com>
2022-10-05 13:42:58 +0200

    MKB-406 - updated venue list (#49)

--- next/components/forms/VenueRentalForm/options.ts
+++ next/components/forms/VenueRentalForm/options.ts
@@ -1,39 +1,39 @@
 import { IFormOption } from '../../../utils/form-constants'
 
 export const options: IFormOption[] = [
+  // {
+  //   key: 'Letná čitáreň U červeného raka, Michalská 26',
+  //   label: [
+  //     { locale: 'sk', label: 'Letná čitáreň U červeného raka, Michalská 26' },
+  //     {
+  //       locale: 'en',
+  //       label: 'Summer Reading Room U Červeného Raka, Michalská 26',
+  //     },
+  //   ],
+  // },
   {
-    key: 'michalska_26',
-    label: [
-      { locale: 'sk', label: 'Letná čitáreň U červeného raka, Michalská 26' },
-      {
-        locale: 'en',
-        label: 'Summer Reading Room U Červeného Raka, Michalská 26',
-      },
-    ],
-  },
-  {
-    key: 'klariska_16',
+    key: 'Pod knižnicou, Klariská 16',
     label: [
       { locale: 'sk', label: 'Pod knižnicou, Klariská 16' },
       { locale: 'en', label: 'Under the Library, Klariská 16' },
     ],
   },
   {
-    key: 'kapucinska_3_galeria',
+    key: 'Galéria Artotéka, Kapucínska 3',
     label: [
       { locale: 'sk', label: 'Galéria Artotéka, Kapucínska 3' },
       { locale: 'en', label: 'Artotéka Gallery, Kapucínska 3' },
     ],
   },
+  // {
+  //   key: 'Kreatívna miestnosť, Kapucínska 3',
+  //   label: [
+  //     { locale: 'sk', label: 'Kreatívna miestnosť, Kapucínska 3' },
+  //     { locale: 'en', label: 'Creative room, Kapucínska 3' },
+  //   ],
+  // },
   {
-    key: 'kapucinska_3_kreativna',
-    label: [
-      { locale: 'sk', label: 'Kreatívna miestnosť, Kapucínska 3' },
-      { locale: 'en', label: 'Creative room, Kapucínska 3' },
-    ],
-  },
-  {
-    key: 'kapucinska_1',
+    key: 'Hudobné štúdio, Kapucínska 1',
     label: [
       { locale: 'sk', label: 'Hudobné štúdio, Kapucínska 1' },
       { locale: 'en', label: 'Music Studio, Kapucínska 1' },
