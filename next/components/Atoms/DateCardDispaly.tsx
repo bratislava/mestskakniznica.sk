92b1045e4d9c03d92dbdcc077ba7fbdebd4fa3bd
Marek Bodinger <marek.bodinger@gmail.com>
2022-12-29 20:15:18 +0100

    Refactor DateCardDispaly

--- next/components/Atoms/DateCardDispaly.tsx
+++ next/components/Atoms/DateCardDispaly.tsx
@@ -1,7 +1,8 @@
+import { now, parseAbsolute } from '@internationalized/date'
+import { bratislavaTimezone } from '@utils/consts'
 import cx from 'classnames'
-
-import { dayForDifferentDateTo } from '../../utils/utils'
-import { usePageWrapperContext } from '../layouts/PageWrapper'
+import { useMemo } from 'react'
+import { useDateFormatter } from 'react-aria'
 
 interface DateCardProps {
   dateFrom: string
@@ -11,14 +12,46 @@ interface DateCardProps {
 }
 
 const DateCardDisplay = ({ dateFrom, dateTo, textSize, wrapperClass }: DateCardProps) => {
-  const { locale } = usePageWrapperContext()
-  const { day, month } = dayForDifferentDateTo(new Date(dateFrom), new Date(dateTo))
+  const formatter = useDateFormatter({
+    day: 'numeric',
+    month: 'numeric',
+    timeZone: bratislavaTimezone,
+  })
 
-  const text = locale == 'en' ? `${month}. ${day}.` : `${day}. ${month}.`
+  const dateToDisplay = useMemo(() => {
+    if (!dateFrom) {
+      return null
+    }
+    const from = parseAbsolute(dateFrom, bratislavaTimezone)
+    if (!dateTo) {
+      return formatter.format(from.toDate())
+    }
+
+    const to = parseAbsolute(dateTo, bratislavaTimezone)
+    const nowZoned = now(bratislavaTimezone)
+
+    const startsBeforeNow = from.compare(nowZoned) < 0
+    const endsAfterNow = to.compare(nowZoned) > 0
+
+    // The event is currently happening. We display the current date.
+    if (startsBeforeNow && endsAfterNow) {
+      return formatter.format(nowZoned.toDate())
+    }
+    // The event is in the future. We display the start date.
+    if (!startsBeforeNow) {
+      return formatter.format(from.toDate())
+    }
+    // The event is in the past. We display the end date.
+    if (!endsAfterNow) {
+      return formatter.format(to.toDate())
+    }
+
+    return null
+  }, [formatter, dateFrom, dateTo])
 
   return (
     <div className={cx('m-auto text-center', wrapperClass)}>
-      <div className={cx(textSize)}>{text}</div>
+      <div className={cx(textSize)}>{dateToDisplay}</div>
       {/* <div className="text-sm capitalize">{month}</div> */}
     </div>
   )
