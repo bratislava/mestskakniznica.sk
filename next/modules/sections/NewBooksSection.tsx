834150bf83cbd2b697a7ca73fad55f16f36510b0
Marek Bodinger <marek.bodinger@gmail.com>
2023-01-04 20:52:30 +0100

    Basic NewBooksSection, will be improved

---
+++ next/modules/sections/NewBooksSection.tsx
@@ -0,0 +1,45 @@
+import { Pagination } from '@components/ui'
+import BookCard from '@modules/common/Cards/BookCard'
+import { newBooksDefaultFilters, newBooksFetcher } from '@utils/fetchers/new-books.fetcher'
+import React, { useState } from 'react'
+import { useQuery } from 'react-query'
+
+/**
+ * Figma: https://www.figma.com/file/CY6Mh2f0SXJhBMY74HdS03/MKB?node-id=4803%3A27424&t=VZjq8OU1CZYXVuDm-0
+ */
+const NewBooksSection = () => {
+  const [filters, setFilters] = useState(newBooksDefaultFilters)
+
+  const { data } = useQuery({
+    queryKey: ['newBooksSection', filters],
+    queryFn: () => newBooksFetcher(filters),
+    keepPreviousData: true,
+  })
+
+  const handlePageChange = (page: number) => {
+    setFilters({ ...filters, page })
+  }
+
+  if (data) {
+    return (
+      <>
+        <div className="mt-8 grid grid-cols-[repeat(auto-fill,minmax(136px,1fr))] gap-y-8 md:grid-cols-[repeat(auto-fill,minmax(196px,1fr))]">
+          {data.books.map((book) => (
+            <BookCard key={book.url} book={book} />
+          ))}
+        </div>
+        <div className="mt-4 flex justify-end">
+          <Pagination
+            max={Math.floor(data.total / filters.pageSize)}
+            value={filters.page}
+            onChangeNumber={handlePageChange}
+          />
+        </div>
+      </>
+    )
+  }
+
+  return null
+}
+
+export default NewBooksSection
