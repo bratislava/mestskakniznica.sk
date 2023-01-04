5d44bc84f64c57cb10de128a939887059d2201b2
Marek Bodinger <marek.bodinger@gmail.com>
2023-01-04 14:44:08 +0100

    BookCard

---
+++ next/components/styleguide/StyleguideCards.tsx
@@ -0,0 +1,24 @@
+import { Stack } from '@components/styleguide/Stack'
+import { Wrapper } from '@components/styleguide/Wrapper'
+import BookCard from '@modules/common/Cards/BookCard'
+import React from 'react'
+
+const StyleguideCards = () => {
+  return (
+    <Wrapper title="Cards" direction="column">
+      <Stack>
+        <BookCard book={{ title: 'Book without cover', url: '' }} />
+        <BookCard
+          book={{
+            title: 'Book with cover',
+            url: '',
+            coverUrl: 'https://coverlinker.biblib.net/?k=ZW5jXzk3ODgwMjAwMjA2MDQtTA==&h=ZW5jXy1M',
+          }}
+        />
+        <BookCard book={{ title: 'Book with very very very very long name', url: '' }} />
+      </Stack>
+    </Wrapper>
+  )
+}
+
+export default StyleguideCards
