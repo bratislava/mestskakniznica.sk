61e6a680153cef0c71cefd2652d8d1f650158bfe
Marek Bodinger <marek.bodinger@gmail.com>
2023-01-06 15:48:08 +0100

    Notice fetchers

---
+++ next/utils/fetchers/notices.fetcher.ts
@@ -0,0 +1,24 @@
+import { client } from '@utils/gql'
+
+export type NoticesFilters = {
+  pageSize: number
+  page: number
+}
+
+export const noticesDefaultFilters: NoticesFilters = {
+  pageSize: 16,
+  page: 1,
+}
+
+export const getNoticesQueryKey = (locale: string, filters: NoticesFilters) => [
+  'notices',
+  locale,
+  filters,
+]
+
+export const noticesFetcher = async (locale: string, filters: NoticesFilters) =>
+  client.Notices({
+    locale,
+    limit: filters.pageSize,
+    start: (filters.page - 1) * filters.pageSize,
+  })
