dcfcc3f42af441f8b8b812191d3ae3eb2022baba
Marek Bodinger <marek.bodinger@gmail.com>
2023-01-06 15:30:41 +0100

    Fix ListingCard

--- next/components/Molecules/ListingCard.tsx
+++ next/components/Molecules/ListingCard.tsx
@@ -2,37 +2,49 @@ import FormatDate from '@modules/common/FormatDate'
 import MLink from '@modules/common/MLink'
 import ShowMoreLink from '@modules/common/ShowMoreLink'
 import { useTranslation } from 'next-i18next'
-import React from 'react'
+import React, { useMemo } from 'react'
 
-import { EventCardEntityFragment, PageEntityFragment } from '../../graphql'
+import { EventCardEntityFragment, NoticeListingEntityFragment } from '../../graphql'
 
 interface IListingCardProps {
-  card: PageEntityFragment | EventCardEntityFragment
+  card: EventCardEntityFragment | NoticeListingEntityFragment
 }
 
 const ListingCard = ({ card }: IListingCardProps) => {
   const { t } = useTranslation('common')
 
-  const linkPrefix = card.__typename === 'EventEntity' ? t('event_slug') : ''
+  const { image, linkPrefix, date } = useMemo(() => {
+    if (card.__typename === 'EventEntity') {
+      return {
+        image: card.attributes?.listingImage?.data,
+        linkPrefix: t('event_slug'),
+        date: card.attributes?.dateFrom,
+      }
+    }
+    if (card.__typename === 'NoticeEntity') {
+      return {
+        image: card.attributes?.listingImage?.data[0],
+        linkPrefix: t('notice_slug'),
+        date: card?.attributes?.dateAdded ?? card.attributes?.publishedAt,
+      }
+    }
+    return { image: null, linkPrefix: null, date: null }
+  }, [card, t])
 
   return (
     <div className="group/showMore relative flex h-full w-full shrink-0 flex-col justify-between">
       <div className="flex h-full flex-col">
-        <img
-          className="h-48 w-full object-cover"
-          alt={t('coverImageFor', { title: card.attributes?.title })}
-          src={card.attributes?.listingImage?.data?.attributes?.url}
-        />
-
-        <div className="mt-4 mb-2 text-sm text-foreground-body">
-          <FormatDate
-            value={
-              card.__typename === 'EventEntity'
-                ? card.attributes?.dateFrom
-                : card.attributes?.date_added ?? card.attributes?.publishedAt
-            }
-            valueType="ISO"
+        {/* TODO: Replace with MImage */}
+        {image && (
+          <img
+            className="mb-4 h-48 w-full object-cover"
+            alt={t('coverImageFor', { title: card.attributes?.title })}
+            src={image.attributes?.url}
           />
+        )}
+
+        <div className="mb-2 text-sm text-foreground-body">
+          <FormatDate value={date} valueType="ISO" />
         </div>
         <MLink
           href={`${linkPrefix}${card.attributes?.slug ?? ''}`}
