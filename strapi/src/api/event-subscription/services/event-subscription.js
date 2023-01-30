8e902877a9575bb8b237acccb35ab254feaae714
Radoslav Zeman <radoslav.zeman@bratislava.sk>
2023-01-30 22:22:46 +0100

    rename Seo to seo, replace by simplified component, remove unused Event Subscriprions

--- strapi/src/api/event-subscription/services/event-subscription.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * event-subscription service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::event-subscription.event-subscription');
