95ab7f67c5218a4fedf9211f5bc9638696bda544
Marek Bodinger <marek.bodinger@gmail.com>
2023-02-02 17:26:01 +0100

    wip

--- next/navikronos/navikronosObject.ts
+++ next/navikronos/navikronosObject.ts
@@ -4,6 +4,10 @@ import { NavikronosClientNavigation, NavikronosConfig, NavikronosStaticProps } f
 
 export type NavikronosObject = {}
 
+type GetRoute = ((type: 'static', id: string, locale?: string) => string | null) &
+  ((entryAlias: string, id: number, locale?: string) => string | null) &
+  ((contentTypeAlias: string, slug: string, locale?: string) => string | null)
+
 export const getNavikronosObject = (
   config: NavikronosConfig,
   // TODO refine
@@ -15,31 +19,35 @@ export const getNavikronosObject = (
   // { type: 'static', id: 'search' }, locale
   // { type: 'page', id: 50 }, locale
   // { type: 'event', slug: 'xyz }, locale
-  const getRoute = ({ type, id, slug, locale }: any) => {
+  const getRoute: GetRoute = (type, idOrSlug, locale) => {
     const localeTree = tree[locale ?? globalLocale]
     if (!localeTree) {
       return null
     }
     if (type === 'static') {
       const map = localeTree.maps.staticRoutesMap2
-      if (map.has(id)) {
-        return map.get(id)?.fullPath
+      if (map.has(idOrSlug as string)) {
+        return map.get(idOrSlug as string)?.fullPath ?? null
       }
     }
     const entryAlias = aliases.entryRoutes.get(type)
     if (entryAlias) {
-      return localeTree.maps.entryRoutesMap2.get([entryAlias, id])?.fullPath
+      return localeTree.maps.entryRoutesMap2.get([entryAlias, idOrSlug as number])?.fullPath ?? null
     }
 
     const contentTypeAlias = aliases.contentTypeRoutes.get(type)
     if (contentTypeAlias) {
-      return localeTree.maps.contentTypeRoutesMap2
-        .get(contentTypeAlias)
-        ?.fullPath?.replace(':slug', slug)
+      return (
+        localeTree.maps.contentTypeRoutesMap2
+          .get(contentTypeAlias)
+          ?.fullPath?.replace(':slug', idOrSlug as string) ?? null
+      )
     }
+
+    return null
   }
 
-  const current = getRoute(currentRoute)
+  const current = getRoute(currentRoute.type, currentRoute.id)
   const getEntity = () => {}
 
   return { tree, currentRoute, getRoute, current }
