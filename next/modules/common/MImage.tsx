390aaa4feb9323347a0b9876f119c3dbe142e3ae
Adam Grund <adam.grund@gmail.com>
2023-02-08 15:54:04 +0100

    MKB-723 fix images

--- next/modules/common/MImage.tsx
+++ next/modules/common/MImage.tsx
@@ -1,5 +1,4 @@
 import { UploadFile } from '@services/graphql'
-import { clouddebugger } from 'googleapis/build/src/apis/clouddebugger'
 import Image from 'next/image'
 import { ComponentProps } from 'react'
 
@@ -14,21 +13,15 @@ type MImageProps = Omit<
 }
 
 // TODO: Placeholder doesn't respect objectFit when used with layout="fill".
-const MImage = ({ image, ...rest }: MImageProps) => {
-  console.log('rest.fill', rest.fill)
-  console.log('image.width', image.width)
-  return (
-    <Image
-      className="object-fill"
-      fill={rest.fill}
-      src={image.url}
-      alt={image.alternativeText ?? ''}
-      // Next shows Image with src "..." and "layout='fill'" has unused properties assigned. Please remove "width" and "height".
-      // width={rest.fill ? undefined : image.width ?? undefined}
-      // height={rest.fill ? undefined : image.height ?? undefined}
-      {...rest}
-    />
-  )
-}
+const MImage = ({ image, ...rest }: MImageProps) => (
+  <Image
+    src={image.url}
+    alt={image.alternativeText ?? ''}
+    // Next shows Image with src "..." and "layout='fill'" has unused properties assigned. Please remove "width" and "height".
+    width={rest.fill ? undefined : image.width ?? undefined}
+    height={rest.fill ? undefined : image.height ?? undefined}
+    {...rest}
+  />
+)
 
 export default MImage
