9bbaefcea9316663b9ad4246f157b6648acd709d
Radoslav Zeman <radoslav.zeman@bratislava.sk>
2023-02-09 16:09:03 +0100

    create new content types for documents and disclosures

---
+++ strapi/src/api/disclosure/routes/disclosure.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * disclosure router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::disclosure.disclosure');
---
+++ strapi/src/api/disclosure/services/disclosure.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * disclosure service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::disclosure.disclosure');
