db15d34de5047440ef17b8320dab9b70ad1d0d43
Marek Bodinger <marek.bodinger@gmail.com>
2023-02-10 16:57:45 +0100

    Fix ports

--- strapi/config/server.js
+++ strapi/config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1338),
+  host: env("STRAPI_HOST", "0.0.0.0"),
+  port: env.int("STRAPI_PORT", 1338),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
-})
+});
