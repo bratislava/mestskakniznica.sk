db2227f182fc5da70622a4cd1fdaef4bc9840465
Marek Bodinger <marek.bodinger@gmail.com>
2023-02-16 11:45:05 +0100

    Fix type

--- next/navikronos/navikronosGetStaticProps.ts
+++ next/navikronos/navikronosGetStaticProps.ts
@@ -1,13 +1,18 @@
 import { GetStaticPropsContext } from 'next/types'
 
 import { fetchNavigation } from './fetchNavigation'
-import { NavikronosConfig, NavikronosStaticProps, RouteEntity } from './types'
+import {
+  NavikronosConfig,
+  NavikronosStaticProps,
+  RouteEntity,
+  RouteEntityWithLocale,
+} from './types'
 
 export const navikronosGetStaticProps = async <Config extends NavikronosConfig>(
   navikronosConfig: Config,
   { locale }: Pick<GetStaticPropsContext, 'locale'>,
   currentEntity?: RouteEntity<Config>,
-  currentEntityLocalizations?: RouteEntity<Config>[]
+  currentEntityLocalizations?: RouteEntityWithLocale<Config>[]
 ) => {
   const { navigation } = await fetchNavigation(navikronosConfig)
 
