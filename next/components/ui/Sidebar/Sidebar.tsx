62a8fb57852ef0b173ac4af02b04f559b95aeb7e
Marek Bodinger <marek.bodinger@gmail.com>
2023-02-21 13:34:40 +0100

    Sidebar using Navikronos

--- next/components/ui/Sidebar/Sidebar.tsx
+++ next/components/ui/Sidebar/Sidebar.tsx
@@ -2,43 +2,39 @@ import { ArrowLeftIcon } from '@assets/icons'
 import ChevronRight from '@assets/images/chevron-right.svg'
 import Button from '@modules/common/Button'
 import MLink from '@modules/common/MLink'
+import { PageEntity } from '@services/graphql'
 import { useNavikronos } from '@utils/navikronos'
 import cx from 'classnames'
 
 export interface SidebarProps {
   className?: string
-  title: string
-  id: string | null
-  categories: ICategory[]
-  activeCategory: number
+  page: PageEntity
 }
 
-export interface ICategory {
-  title: string
-  id: string | null
-}
-
-export const Sidebar = ({ className, title, id, categories, activeCategory }: SidebarProps) => {
-  const { getPathForEntity } = useNavikronos()
+export const Sidebar = ({ className, page }: SidebarProps) => {
+  const { siblings, currentPath, getPathForEntity } = useNavikronos()
 
   return (
     <div className={className}>
       {/* TODO create component for this button or update Button variants */}
-      <Button
-        href={getPathForEntity({ type: 'page', id }) ?? '#'}
-        variant="unstyled"
-        startIcon={<ArrowLeftIcon />}
-        className="inline-flex items-center gap-x-4 text-base uppercase"
-      >
-        {title}
-      </Button>
+      {page && (
+        <Button
+          href={getPathForEntity({ type: 'page', id: page.id }) ?? '#'}
+          variant="unstyled"
+          startIcon={<ArrowLeftIcon />}
+          className="inline-flex items-center gap-x-4 text-base uppercase"
+        >
+          {page.attributes?.title}
+        </Button>
+      )}
       <div className={cx('flex flex-col')}>
-        {categories.map((category, i) => {
-          const isActive = activeCategory === i
+        {siblings.map((child, index) => {
+          const isActive = child.path === currentPath
           return (
             <MLink
-              key={category.title}
-              href={getPathForEntity({ type: 'page', id: category.id }) ?? '#'}
+              // eslint-disable-next-line react/no-array-index-key
+              key={index}
+              href={child.path}
               variant="basic"
               className={cx('transform border-b py-3 transition-all duration-200 ease-linear', {
                 'border-border-light text-foreground-body': !isActive,
@@ -47,7 +43,7 @@ export const Sidebar = ({ className, title, id, categories, activeCategory }: Si
               })}
             >
               {isActive && <ChevronRight />}
-              {category.title}
+              {child.title}
             </MLink>
           )
         })}
