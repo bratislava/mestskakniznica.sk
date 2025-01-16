818b55e53145b8e9730b0ddf3cab3987e5aa48d1
Katarína Zápražná <katarina.zaprazna@bratislava.sk>
2025-01-16 15:49:20 +0100

    Make adjustments to the form

--- next/components/forms/ExcursionReservationForm/options.ts
+++ next/components/forms/ExcursionReservationForm/options.ts
@@ -4,40 +4,10 @@ export const options: IFormOption[] = [
   {
     key: 'spoznajte_knizicu_exkurzia',
     label: [
-      { locale: 'sk', label: 'Spoznajte knižnicu po svojom – Exkurzia' },
+      { locale: 'sk', label: 'Spoznajte knižnicu po svojom: Exkurzia' },
       {
         locale: 'en',
-        label: 'Get to know the library in your own way - Excursion',
-      },
-    ],
-  },
-  {
-    key: 'spoznajte_knizicu_exkurzia_prehliadka_letnej_citarne',
-    label: [
-      {
-        locale: 'sk',
-        label:
-          'Spoznajte knižnicu po svojom – Exkurzia a prehliadka Letnej čitárne U červeného raka',
-      },
-      {
-        locale: 'en',
-        label:
-          'Get to know the library in your own way - Excursion and tour of the Summer Reading Room U červeného raka',
-      },
-    ],
-  },
-  {
-    key: 'spoznajte_knizicu_komentovana_prehliadka_letnej_citarne',
-    label: [
-      {
-        locale: 'sk',
-        label:
-          'Spoznajte knižnicu po svojom – Komentovaná prehliadka Letnej čitárne U červeného raka',
-      },
-      {
-        locale: 'en',
-        label:
-          'Get to know the library in your own way - Guided tour of the Summer Reading Room U červeného raka',
+        label: 'Get to know the library in your own way: Excursion',
       },
     ],
   },
@@ -46,33 +16,59 @@ export const options: IFormOption[] = [
     label: [
       {
         locale: 'sk',
-        label: 'Spoznajte knižnicu po svojom – Exkurzia a knižný lov',
+        label: 'Spoznajte knižnicu po svojom: Knižný lov',
       },
       {
         locale: 'en',
-        label: 'Get to know the library in your own way - Excursion and book hunt',
+        label: 'Get to know the library in your own way: Excursion and book hunt',
       },
     ],
   },
   {
-    key: 'patrim_do_kniznice_vitanie_prvacikov',
+    key: 'patrim_do_kniznice_vitanie_prvaceniec',
     label: [
-      { locale: 'sk', label: 'Patrím do knižnice! – vítanie prváčikov' },
-      { locale: 'en', label: 'I belong in the library! - welcoming freshmen' },
+      { locale: 'sk', label: 'Patrím do knižnice! - Vítanie prváčeniec' },
+      { locale: 'en', label: 'I belong to the library! - Welcoming first-graders' },
     ],
   },
   {
     key: 'po_stopach_staromestianskych_povesti',
     label: [
-      { locale: 'sk', label: 'Po stopách staromeštianskych povestí' },
-      { locale: 'en', label: 'In the footsteps of the Old Town tales' },
+      { locale: 'sk', label: 'Po stopách staromeštianských povestí' },
+      { locale: 'en', label: 'In the footsteps of Old Town legends' },
+    ],
+  },
+  {
+    key: 'po_stopach_staromeskeho_opevnenia',
+    label: [
+      {
+        locale: 'sk',
+        label: 'Po stopách staromestského opevnenia',
+      },
+      {
+        locale: 'en',
+        label: 'In the footsteps of city fortifications',
+      },
     ],
   },
   {
     key: 'svet_pana_brailla',
     label: [
       { locale: 'sk', label: 'Svet pána Brailla' },
-      { locale: 'en', label: "Mr Braille's World" },
+      { locale: 'en', label: "Mr. Braille's World" },
+    ],
+  },
+  {
+    key: 'citanie_so_psikmi',
+    label: [
+      {
+        locale: 'sk',
+        label: 'Čítanie so psíkmi',
+      },
+      {
+        locale: 'en',
+        label: 'Reading with doggies',
+      },
     ],
   },
 ]
