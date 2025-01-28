57da76c39f7a1a59de433a88b4b0bda26ddabb22
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2025-01-28 14:19:31 +0100

    #344 Show banner with current change of opening hours (#359)
    * #344 Add `isCurrentChangeInOpeningHours` into Notices and remove it from BlogPosts
    
    * #344 Add alert with current changes in opening hours
    
    * Use ShowMoreLink component
    
    * Add aria-labelledby attributes
    
    * Update query key to OpeningHoursChangeNotices
    
    * Fix ordering by publishedAt

--- next/modules/sections/OpeningHoursSection.tsx
+++ next/modules/sections/OpeningHoursSection.tsx
@@ -1,22 +1,31 @@
 import BranchOpeningHours from '@components/Molecules/BranchDetails/ContactUsOpeningHours/BranchOpeningHours'
+import OpeningHoursChangeAlert from '@components/Molecules/OpeningHoursChangeAlert'
 import { OpeningHoursSectionFragment } from '@services/graphql'
 import { isDefined } from '@utils/isDefined'
 import React from 'react'
 
 const OpeningHoursSection = ({ title, branchList }: Omit<OpeningHoursSectionFragment, 'id'>) => {
+  const filteredBranches = branchList?.data.filter(isDefined) ?? []
+
   return (
-    <div className="">
+    <div className="flex flex-col gap-8">
       {title && <h2 className="text-h3">{title}</h2>}
-      {branchList?.data.filter(isDefined).map((branch) => {
-        return (
-          <div className="border-t border-border-dark py-6 first:pt-0 first-of-type:border-none lg:py-8">
-            <h3 className="text-h4">{branch.attributes?.title}</h3>
-            <div className="mt-4">
-              <BranchOpeningHours days={branch.attributes?.openingHours?.days ?? []} />
-            </div>
-          </div>
-        )
-      })}
+      <OpeningHoursChangeAlert />
+      {filteredBranches.length > 0 ? (
+        <div className="flex flex-col gap-6 lg:gap-8">
+          {filteredBranches.map((branch, index) => {
+            return (
+              <>
+                {index > 0 && <div className="border-t border-border-dark" aria-hidden />}
+                <div className="flex flex-col gap-4">
+                  <h3 className="text-h4">{branch.attributes?.title}</h3>
+                  <BranchOpeningHours days={branch.attributes?.openingHours?.days ?? []} />
+                </div>
+              </>
+            )
+          })}
+        </div>
+      ) : null}
     </div>
   )
 }
