5c1d47b0cb36e46ca6e0c2ed4b6b43853e54fe49
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2025-01-31 06:37:00 +0100

    #345 Setup i18next-parser

--- next/next-i18next.config.js
+++ next/next-i18next.config.js
@@ -1,3 +1,5 @@
+// This config is used by next-i18next. The `i18n` "subconfig" should be also imported and used in next.config.js.
+// Docs: https://github.com/i18next/next-i18next?tab=readme-ov-file#3-project-setup
 module.exports = {
   i18n: {
     defaultLocale: 'sk',
