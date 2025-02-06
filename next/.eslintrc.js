93f8aa624f6065eed404509c1f29540867175155
Katarína Zápražná <katarina.zaprazna@bratislava.sk>
2025-02-06 21:40:25 +0100

    Update and unify configs across repositories

--- next/.eslintrc.js
+++ next/.eslintrc.js
@@ -1,24 +1,58 @@
 module.exports = {
-  extends: ['auto', 'plugin:tailwindcss/recommended', 'plugin:@next/next/recommended'],
-  plugins: ['only-warn'],
+  extends: [
+    'auto',
+    'plugin:tailwindcss/recommended',
+    'plugin:@next/next/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:lodash/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  plugins: [
+    'only-warn',
+    'prettier',
+    'jsx-a11y',
+    'lodash',
+    '@typescript-eslint',
+    '@next/next',
+    'simple-import-sort',
+    'tailwindcss',
+  ],
+  parser: '@typescript-eslint/parser', // Ensure correct parser for TypeScript
   rules: {
     'react/react-in-jsx-scope': 'off',
-    /** We use this a lot with isDefined and hasAttributes */
-    'unicorn/no-array-callback-reference': 'off',
-    /** Named export is easier to refactor automatically */
-    'import/prefer-default-export': 'off',
-    /** Too tedious to type every function return explicitly */
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    /** We prefer arrow functions */
-    'react/function-component-definition': [2, { namedComponents: 'arrow-function' }],
-    /** It's annoying to refactor from one style to another */
-    'arrow-body-style': 'off',
-    /** This are exceptions that we use with "__" */
+    'react/function-component-definition': [2, { namedComponents: 'arrow-function' }], // We prefer arrow functions
+    'react/require-default-props': 'off', // We specify default props in props decomposition
+
+    /** TypeScript */
+    '@typescript-eslint/explicit-function-return-type': 'off', // Too tedious to type every function return explicitly
+    '@typescript-eslint/no-unsafe-assignment': 'off',
+    '@typescript-eslint/no-unsafe-call': 'off',
+    '@typescript-eslint/no-unsafe-return': 'off',
+    '@typescript-eslint/no-unsafe-member-access': 'off',
+    '@typescript-eslint/no-unsafe-argument': 'off',
+
+    /** Accessibility */
+    'jsx-a11y/img-redundant-alt': 'warn',
+    'jsx-a11y/anchor-is-valid': 'off', // Next Link does not need href in <a> tag
+
+    /** Imports */
+    'import/prefer-default-export': 'off', // Named export is easier to refactor automatically
+    'import/extensions': 'off',
+
+    'prettier/prettier': 'error',
+    'unicorn/no-array-callback-reference': 'off', // We use this a lot with isDefined and hasAttributes
+    'arrow-body-style': 'off', // It's annoying to refactor from one style to another
+    'lodash/prefer-noop': 'off',
+    '@next/next/no-img-element': 'off',
+
+    // These are exceptions that we use with "__"
     'no-underscore-dangle': [
       2,
       { allow: ['__NEXT_DATA__', '__NEXT_LOADED_PAGES__', '__typename'] },
     ],
-    /** Links get confused for secrets */
+
+    // Links get confused for secrets
     'no-secrets/no-secrets': [
       'error',
       {
@@ -28,25 +62,10 @@ module.exports = {
         ],
       },
     ],
-    /** We specify default props in props decomposition */
-    'react/require-default-props': 'off',
-    /** Next Link does not need href in <a> tag */
-    'jsx-a11y/anchor-is-valid': 'off',
-    /** Do not work in our case */
-    '@typescript-eslint/no-unsafe-assignment': 'off',
-    '@typescript-eslint/no-unsafe-call': 'off',
-    '@typescript-eslint/no-unsafe-return': 'off',
-    '@typescript-eslint/no-unsafe-member-access': 'off',
-    '@typescript-eslint/no-unsafe-argument': 'off',
-    'lodash/prefer-noop': 'off',
-    'jsx-a11y/img-redundant-alt': 'warn',
-    '@next/next/no-img-element': 'off',
+
+    /** Does not work in our case */
     // https://github.com/jsx-eslint/eslint-plugin-react/issues/2584#issuecomment-1191175244
     'react/jsx-no-useless-fragment': [2, { allowExpressions: true }],
-    'import/extensions': 'off',
-
-    /* Formatting rules - based on OLO */
-    'prettier/prettier': ['error', { endOfLine: 'auto' }], // TODO revisit, prettier should not be run by eslint
   },
   ignorePatterns: ['*.config.*', 'graphql', '.eslintrc.js'],
 }
--- next/.prettierrc.js
+++ next/.prettierrc.js
@@ -1,9 +1,9 @@
 module.exports = {
-  trailingComma: 'all',
+  trailingComma: 'es5',
   tabWidth: 2,
   semi: false,
   singleQuote: true,
   printWidth: 100,
   plugins: ['prettier-plugin-tailwindcss'],
-  tailwindFunctions: ['cx', 'classnames', 'clsx', 'cn', 'twMerge', 'tw'],
+  tailwindFunctions: ['clsx', 'cn', 'twMerge'],
 }
