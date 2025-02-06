285adec94a835b03175fe18b19b6ef59c1bd3c80
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2025-02-06 14:31:14 +0100

    Upgrade typescript, next, sharp

--- next/modules/common/MDatePicker/MDatePicker.tsx
+++ next/modules/common/MDatePicker/MDatePicker.tsx
@@ -3,15 +3,15 @@ import 'react-datepicker/dist/react-datepicker.css'
 import enIE from 'date-fns/locale/en-IE'
 import sk from 'date-fns/locale/sk'
 import { useTranslation } from 'next-i18next'
-import React, { ComponentProps } from 'react'
-import ReactDatePicker, { registerLocale } from 'react-datepicker'
+import React from 'react'
+import ReactDatePicker, { DatePickerProps, registerLocale } from 'react-datepicker'
 
 /* https://unix.stackexchange.com/a/62317
  * https://github.com/date-fns/date-fns/issues/1996#issuecomment-984811417 */
 registerLocale('en', enIE)
 registerLocale('sk', sk)
 
-const MDatePicker = (props: ComponentProps<typeof ReactDatePicker>) => {
+const MDatePicker = (props: DatePickerProps) => {
   const { i18n } = useTranslation()
 
   return <ReactDatePicker locale={i18n.language} {...props} className="base-focus-ring" />
