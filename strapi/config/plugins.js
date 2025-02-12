8b328d77eed46e1152295a1b942421f12234b479
jankaBehal <jana.dadova@bratislava.sk>
2025-02-12 17:57:10 +0100

    #351 Setup email provider to be able to send reset password to admin (#388)
    * Mailgun initial setup
    
    * Add missing env variables

--- strapi/config/plugins.js
+++ strapi/config/plugins.js
@@ -118,6 +118,21 @@ module.exports = ({ env }) => ({
       ],
     },
   },
+  email: {
+    config: {
+      //  mailgun email provider https://www.npmjs.com/package/@strapi/provider-email-mailgun
+      provider: 'mailgun',
+      providerOptions: {
+        key: env('MAILGUN_API_KEY'), // Required
+        domain: env('MAILGUN_DOMAIN'), // Required
+        url: 'https://api.eu.mailgun.net', //Optional. If domain region is Europe use 'https://api.eu.mailgun.net'
+      },
+      settings: {
+        defaultFrom: env('MAILGUN_EMAIL'),
+        defaultReplyTo: env('MAILGUN_EMAIL')
+      },
+    },
+  },
   meilisearch: {
     config: {
       host: process.env.MEILISEARCH_HOST,
