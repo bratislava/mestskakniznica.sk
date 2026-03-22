eb4d570d9cf5052866dde3df0760ed13c54f1cc5
Adam <jakubicka.adam@gmail.com>
2026-03-22 23:33:20 +0100

    Fix NewsletterSection paddings

--- next/components/HomePage/NewsletterSection.tsx
+++ next/components/HomePage/NewsletterSection.tsx
@@ -2,6 +2,7 @@ import { FormProvider } from 'react-hook-form'
 
 import { useNewsletterSection } from '@/components/HomePage/useNewsletterSection'
 import NewsLetter from '@/components/ui/NewsLetter/NewsLetter'
+import { SectionContainer } from '@/components/ui/SectionContainer/SectionContainer'
 
 /**
  * Figma: https://www.figma.com/design/CY6Mh2f0SXJhBMY74HdS03/Mestsk%C3%A1-kni%C5%BEnica--MKB-?node-id=10907-4250&t=KkuqLqbD8kvacbyQ-4
@@ -12,11 +13,16 @@ const NewsletterSection = () => {
 
   return (
     <FormProvider {...methods}>
-      <NewsLetter
-        onSubmit={handleSubmit}
-        respondMessage={responseMessage}
-        resStatus={responseStatus}
-      />
+      <SectionContainer
+        //
+        className="py-12 lg:py-24"
+      >
+        <NewsLetter
+          onSubmit={handleSubmit}
+          responseMessage={responseMessage}
+          responseStatus={responseStatus}
+        />
+      </SectionContainer>
     </FormProvider>
   )
 }
