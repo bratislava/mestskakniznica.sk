cde6078f2e5f72ec41fb5ed5f5995708a89c3315
Ty-ci <teachy.jaho@gmail.com>
2026-06-15 10:56:15 +0200

    Upgrade small majors batch

--- next/modules/common/ImageGallery/GalleryModal.tsx
+++ next/modules/common/ImageGallery/GalleryModal.tsx
@@ -1,4 +1,4 @@
-﻿import FocusTrap from 'focus-trap-react'
+﻿import { FocusTrap } from 'focus-trap-react'
 import { AnimatePresence, motion } from 'framer-motion'
 import { useTranslation } from 'next-i18next/pages'
 import { ReactNode, useEffect, useRef } from 'react'
